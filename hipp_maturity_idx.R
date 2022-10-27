192f41be9f7ff0ce2937677be75fa4c191fcf4dd
Jonah Kember <81769550+JonahKember@users.noreply.github.com>
2022-10-27 16:22:44 -0400

    Update hipp_maturity_idx.R

--- hipp_maturity_idx.R
+++ hipp_maturity_idx.R
@@ -2,6 +2,12 @@
 # Hippocampus maturity index
 #############################
 
+# Specify whether hippocampal volume should be controlled for.
+vol_ctrl = T
+
+# Specify whether the Hippocampus Maturity Index should be calculated using all subjects, or only those with Pic-Seq scores.
+inclusion = T
+
 # Path with cleaned data from clean_data.R
 path = 'C:\\Users\\jonah\\Documents\\MAGeT\\data\\'
 
@@ -17,16 +23,20 @@ library(ggplot2)
 # Specify data.
 data = lh_data
 
-# Find PLS components (direction that maximizes covariance between volume matrix and age).
-pls_fit = plsr(Age ~ CA1 + Subiculum + CA4.DG + CA3.CA2 + SRLM, data = data, scale = T)
-summary(pls_fit)
-
-# Test the correlation between each component and picture sequence scores.
-for (var in 1:5){
-  print(cor.test(data$Picture.Seq..Scores, pls_fit$scores[,var]))
+if(vol_ctrl == T){
+  tot_vol = apply(data[,3:7],1,sum)
+  data[,3:7] = data[,3:7]/tot_vol
 }
 
-# Plot the correlation between the 1st PLS component and Picture-Sequence scores.
+if(inclusion == T){
+  include = which(data$Picture.Seq..Scores > 0)
+  data = data[include,]
+}
+
+# Find PLS components (directions that maximize covariance between volume matrix and age).
+pls_fit = plsr(Age ~ CA1 + Subiculum + CA4.DG + CA3.CA2 + SRLM, data = data, scale = T)
+
+# Plot relationship between the 1st PLS component and Picture-Sequence scores..
 ggplot(data = data, aes(x = pls_fit$scores[,1], y = Picture.Seq..Scores)) + 
   geom_point() +
   theme_classic() +
@@ -39,4 +49,14 @@ ggplot(data = data, aes(x = pls_fit$scores[,1], y = Picture.Seq..Scores)) +
 ggsave(paste(path,'lh_hipp_maturity_pic_seq.png', sep = ''), plot = last_plot(),
        width = 8, height = 5)
 
+# Calculate total variance explained.
+pls_loadings =  pls_fit$loadings[,1]
+X_var = pls_fit$Xvar/pls_fit$Xtotvar[1]
+Y_var = cor(pls_fit$scores,data$Age)[1]^2
 
+# Calculate correlation between hippocampal maturity and memory.
+cor_results = cor.test(data$Picture.Seq..Scores, pls_fit$scores[,1])
+
+cat('Total variance in X:',X_var[1]*100,'%. \nTotal variance in Y:',Y_var*100,'%.')
+cat('Correlation between hippocampal maturity and picture-sequence scores: r =',cor_results$estimate,'p =',cor_results$p.value)
+cat('PLS loadings:',pls_fit$loadings[,1])
