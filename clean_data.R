6b7fcb6decf16869e0c92e8df24ab5e2419ca86a
Jonah Kember <81769550+JonahKember@users.noreply.github.com>
2022-10-31 12:57:01 -0400

    Update clean_data.R
    Added and mean-imputed intracranial volumes to cleaned data frame.

--- clean_data.R
+++ clean_data.R
@@ -9,17 +9,17 @@ rh_data = read.csv(paste(path,'rh_volumes_demographics.csv',sep = ''))
 lh_data = read.csv(paste(path,'lh_volumes_demographics.csv',sep = ''))
 
 # Remove irrelevant data (raw scores, redundant age/income/ID variables).
-rh_data = rh_data[,c(2:7,9:12,15,19,22)]
-lh_data = lh_data[,c(2:7,9:12,15,19,22)]
+rh_data = rh_data[,c(2:7,9:12,15,19,22,23)]
+lh_data = lh_data[,c(2:7,9:12,15,19,22,23)]
 
 # Rename columns.
 colnames(rh_data) = c('ID','CA1','Subiculum','CA4/DG','CA3/CA2','SRLM',
                       'Age','Sex','Site','Income',
-                      'IQ (MR)','Picture Seq. Scores','Picture Seq. Theta')
+                      'IQ (MR)','Picture Seq. Scores','Picture Seq. Theta','ICV')
 
 colnames(lh_data) = c('ID','CA1','Subiculum','CA4/DG','CA3/CA2','SRLM',
                       'Age','Sex','Site','Income',
-                      'IQ (MR)','Picture Seq. Scores','Picture Seq. Theta')
+                      'IQ (MR)','Picture Seq. Scores','Picture Seq. Theta','ICV')
 
 # Transform to data-frames.
 rh_data = as.data.frame(rh_data)
@@ -34,11 +34,18 @@ lh_data$'Picture Seq. Scores'[lh_data$'Picture Seq. Scores' == 0] = NA
 lh_data$'Picture Seq. Theta'[lh_data$'Picture Seq. Theta' == 0] = NA
 lh_data$Income[lh_data$Income < 0] = NA
 
+# Perform mean imputation for missing intracranial volumes.
+rh_mean_icv = mean(rh_data$ICV, na.rm = T)
+rh_data$ICV[is.nan(rh_data$ICV)] = rh_mean_icv
+
+lh_mean_icv = mean(lh_data$ICV, na.rm = T)
+lh_data$ICV[is.nan(lh_data$ICV)] = lh_mean_icv
+
 # Visualize standardized data.
 z_score <- function(x) {(x - mean(x, na.rm = T))/sd(x, na.rm = T)}
   
-z_rh_data = apply(rh_data[,2:13],2,z_score)
-z_lh_data = apply(lh_data[,2:13],2,z_score)
+z_rh_data = apply(rh_data[,2:14],2,z_score)
+z_lh_data = apply(lh_data[,2:14],2,z_score)
 
 # Create box plots, exclude nominal vars (sex, site).
 boxplot(z_rh_data[,c(-7,-8)], ylab = 'Z-Score')
