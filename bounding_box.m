69eb9f14da6ab196bd39f32262a4a29cf84a2a5b
Jonah Kember <81769550+JonahKember@users.noreply.github.com>
2022-12-21 09:15:43 -0500

    Update bounding_box.m

--- bounding_box.m
+++ bounding_box.m
@@ -1,15 +1,14 @@
 function box = bounding_box(vol, boundary_add, output)
+% Inputs:
+%   vol             = Volume with object to be masked (3D-array, .nii, .nii.gz).
+%   boundary_add    = Dimensions to be added to extremes of bounding box. 
+%   output          = File name for output .nii.
 
 % Create bounding box around given object (vol: 3D array or .nii). Assumes elements == 0 are
 % to be excluded from object, while voxels > 0 are to be included. boundary_add specifies amount
 % of space to be added along each dimension of box. If vol is .nii, boundary_add works in voxel
 % size; if vol is a 3D array, boundary_add works in number of voxels. If boundary_add is a scalar,
 % it is added to each dimension; if boundary_add is length(3), elements are added along x,y,z directions.
-
-% Inputs:
-%   vol             = Volume with object to be masked (3D-array, .nii, .nii.gz).
-%   boundary_add    = Dimensions to be added to extremes of bounding box. 
-%   output          = File name for output .nii.
 
 if length(boundary_add) == 1
     boundary_add = repelem(boundary_add,3);
